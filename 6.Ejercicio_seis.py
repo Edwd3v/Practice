d6a3a6795ddf3cb1112b2a575ef69d541769e2de
Edwd3v <EdwardSTM1023@gmail.com>
2023-03-14 21:53:51 -0500

    Se anade '6.Ejercicio_seis.pt' al repositorio.

---
+++ 6.Ejercicio_seis.py
@@ -0,0 +1,29 @@
+
+# Escriba un programa que permita crear una lista de palabras y que, a continuación, cree una segunda lista igual a la primera, pero al revés (no se trata de escribir la lista al revés, sino de crear una lista distinta).
+
+def run():
+
+    num_list = int(input('Dígame cuántas palabras tiene la lista: '))
+    strings_list = []
+    invers_string_list = []
+        
+    if num_list <= 0:
+        print('¡Imposible!')
+        return False
+
+    for i in range (num_list):
+        if num_list > 0:
+            element = input('Dígame la palabra {}: '.format(i+1))
+            strings_list.append(element)
+    
+    print('La lista creada es: {}'.format(strings_list))
+
+    for i in reversed(strings_list):
+        invers_string_list.append(i)
+    
+    print('La lista inversa es: {}'.format(invers_string_list))
+
+
+
+if __name__ == '__main__':
+    run()
